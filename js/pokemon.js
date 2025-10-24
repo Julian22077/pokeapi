88f0e5beac9b744f77f8373d6de6c4f593b65171
Julian22077 <camilolozano0122@gmail.com>
2025-10-24 02:41:37 +0000

    tridecimo commit

--- js/pokemon.js
+++ js/pokemon.js
@@ -46,38 +46,41 @@ async function Pokemon(id){
         
         let tipoPoke=""
         for(let i=0;i<data.types.length;i++){
-            tipoPoke +=`<span>${data.types[i].type.name} 0</span>`;
+            tipoPoke +=`<span>${data.types[i].type.name} </span>`;
         }
-        document.getElementById("uno").classList.add(data.types[0].type.name)
-        root.innerHTML=` <section class="c-detalle">
-        <div class="detalle-header">
-            <button id="Volver><</button>
-            <img  class= "pokemonbrand"  src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg">
-            <img  class="pokeball" src="https://cdn-icons-png.flaticon.com/512/1169/1169608.png ">
+       colorFondo = data.types[0].type.name
+        document.getElementById("uno").classList.add(colorFondo)
+        root.innerHTML=` 
+        <section class="c-detalle">
+       <div class="detalle-header">
+            <button id="volver"><</button>
+            <img class="pokemonbrand" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png">
+            <img class="" src="https://cdn-icons-png.flaticon.com/512/1169/1169659.png">
         </div>
 
-        <div class="nombre>
-        <p>${data.name}</p>
-        <p>${tipoPoke}</p>
-        </div>  
+        <div class="nombre">
+            <h3>${data.name}</h3>
+            <p>${tipoPoke}</p>
+        </div>
+        <img class="img-poke" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png" alt="${data.name}" height="120" width="auto">
+        
 
         <div class="detalle-informacion">
-        <button class="about" onClick="mostrarinformacion()" >About</button>
+        <button class="about" onClick="mostrarinformacion()">About</button>
         <button class="favorito" onClick="toggleFavorito(${data.id}, '${data.name}')">
-         <span id="corazon-${data.id}">${esFavorito ? '❤️' : '🤍'}</span> Favorito
-      </button>
-      <button class="evoloution">Evolution</button>
-      </div>
-      <div class="informacion" id="informacion">
-      <p>${data.id}</p>
-      <p>Altura: ${data.height / 10} m / Peso: ${data.weight / 10} kg</p>
+        <span id="corazon-${data.id}">${esFavorito ? '❤️' : '🤍'}</span>
+        </button>
+        <button class="evolution">Evolution</button>
+        </div>
+        
+        <div id="informacion" class="informacion">
+        <p>${data.id}</p>
+        <p>Altura: ${data.height / 10} m / Peso: ${data.weight / 10} kg</p>
         <p>hp: ${data.stats[0].base_stat}</p>
         <p>Velocidad: ${data.stats[5].base_stat}</p>
         <p>Ataque: ${data.stats[1].base_stat} Defensa: ${data.stats[2].base_stat}</p>
         <p>Ataque Especial: ${data.stats[3].base_stat} Defensa Especial: ${data.stats[4].base_stat}</p>
         </div>
-
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png" alt="${data.name}" height="120" width="auto">
 
 
     </section>
